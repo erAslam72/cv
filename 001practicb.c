1d0dad5702657bc272b0d6c594669d63067b78b9
erAslam72 <125135802+erAslam72@users.noreply.github.com>
2023-02-11 19:23:10 +0530

    Add files via upload

---
+++ 001practicb.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include<conio.h>
+#include <string.h>
+int main()
+{
+    // int leght, breath;
+
+    // printf("what is the value of lenght is \n");
+    // scanf("%d", &leght);
+    // printf(" enter the value of breath is \n");
+    // scanf("%d",& breath);
+    // int area = leght * breath;
+    // printf("area of this rectunghl is %d\n", area);
+//     char wi ;
+//      char si;
+//     printf("whis the value width is \n");
+//     scanf("%c",& wi);
+
+//    printf("whis the value width is \n");
+//     scanf("%c", & si);
+//     char an = wi + si;
+//     printf("area of rectanglr isc %c\n", an);
+
+
+char g='h';
+   char h='g';
+  // char j;
+
+    printf("ch = %c\n",g);
+
+  printf("ch = %c\n",h);
+
+
+     strcat(g, h);
+strchr(g,h);
+ return 0;
+}
